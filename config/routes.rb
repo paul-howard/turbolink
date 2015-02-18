1eb09d47943f723ee6dc59dba077dcaf25bee083
Paul Howard <paul@paul-howard.net>
2015-02-18 11:27:11 -0800

    Add site & session controllers; add rails-bootstrap-form gem; move sqlite3 gem to dev/test only; update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  ## Root
+  root "site#index"
+
+  ## Generated Resource Routes
   resources :users
+
+  ## Custom Routes
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
