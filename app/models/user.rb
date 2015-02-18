98b35391a888364e4111e5df1af753484e6a2425
Paul Howard <paul@paul-howard.net>
2015-02-17 22:58:33 -0800

    make basic password auth work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+
+  validates :email,
+    presence: true,
+    uniqueness: {case_sensitive: false}
+
+  validates :phone_num,
+    presence: true,
+    uniqueness
+
+  has_secure_password
+
+  validates_confirmation_of :password on: :create
+  validates_presence_of :password_confirmation
+
 end
